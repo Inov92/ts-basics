5f34618bbd2251f5b4282394351dde57c1b71342
Protasov Ivan (ext) RUSP <ivan.protasov@syngenta.com>
2024-03-05 13:24:04 +0300

    Code formatting

--- src/typesAndObjects.ts
+++ src/typesAndObjects.ts
@@ -1,9 +1,9 @@
-let user: {id: number; name: string};
+let user: { id: number; name: string };
 
 // You can use , or ; to separate the properties, and the last separator is optional either way.
 user = {
-    id: 1,
-    name: 'Ivan'
+  id: 1,
+  name: "Ivan",
 };
 
 //Compilation error. id typy is wrong, name is mandatory
@@ -12,16 +12,16 @@ user = {
 // }
 
 //Optional properties are marked with :?
-let book: {name: string; iban?: boolean}
+let book: { name: string; iban?: boolean };
 
-book = {name: "Puss in Boots"};
+book = { name: "Puss in Boots" };
 
 if (book.iban === undefined) {
-    console.log("Optional property is allowed by ?:");
-};
+  console.log("Optional property is allowed by ?:");
+}
 
 // *** ARRAYS ***
-let heroes : string[] = [];
+let heroes: string[] = [];
 heroes.push("Ayaka");
 heroes.push("Yoimiya");
 //heroes.push(112); Error due to type check
